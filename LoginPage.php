030976ec3558d70a57df8f5f6fffdb5d71a071a4
sawoo9410 <sawoo9410@gmail.com>
2019-05-03 17:17:20 +0900

    20190503 Login succes

---
+++ LoginPage.php
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+<title>Loginpage</title>
+
+
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
+<link rel="stylesheet" href="css/LoginStyle.css">
+
+
+</head>
+
+<body>
+
+
+<form method="post" action="Login_action.php">
+    <div class="login-wrap">
+        <div class="login-html">
+            <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">학생/교수</label>
+            <input id="tab-2" type="radio" name="tab" class="administrator"><label for="tab-2" class="tab">관리자</label>
+            <div class="login-form">
+                <div class="sign-in-htm">
+                    <div class="group">
+                        <label for="user" class="label">Username</label>
+                        <input id="user" name ="login_id" type="text" class="input">
+                    </div>
+                    <div class="group">
+                        <label for="pass" class="label">Password</label>
+                        <input id="pass" name="login_pw" type="password" class="input" data-type="password">
+                    </div>
+                    <div class="group">
+                        <input id="check" type="checkbox" class="check" checked>
+                        <label for="check"><span class="icon"></span> 아이디 기억</label>
+                    </div>
+                    <div class="group">
+                        <input type="submit" class="button" value="Sign In">
+                    </div>
+                    <div class="hr"></div>
+                </div>
+                <div class="administrator-htm">
+                    <div class="group">
+                        <label for="user" class="label">Username</label>
+                        <input id="user" type="text" class="input">
+                    </div>
+                    <div class="group">
+                        <label for="pass" class="label">Password</label>
+                        <input id="pass" type="password" class="input" data-type="password">
+                    </div>
+                    <div class="group">
+                        <input id="check" type="checkbox" class="check" checked>
+                        <label for="check"><span class="icon"></span>아이디 기억</label>
+                    </div>
+                    <div class="group">
+                        <input type="submit" class="button" value="Sign In">
+                    </div>
+                    <div class="hr"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</form>
+</body>
+
+</html>
---
+++ Login_action.php
@@ -0,0 +1,47 @@
+﻿<?php
+echo "test<br>";
+session_start();
+$connect = mysqli_connect("localhost:3307", "root", "secret", "smu");
+
+$id=$_POST['login_id'];
+$pw=$_POST['login_pw'];
+
+$query = "select * from smu_member where smu_id='$id'";
+$result = $connect->query($query);
+
+
+if(mysqli_num_rows($result)==1) {
+
+$row=mysqli_fetch_assoc($result);
+
+//비밀번호가 맞다면 세션 생성
+if($row['smu_Id']==$pw){
+$_SESSION['login_id']=$id;
+    if(isset($_SESSION['login_id'])){
+        ?>      <script>
+            alert("로그인 되었습니다.");
+            location.replace("./homepage.html");
+        </script>
+        <?php
+    }
+    else{
+        echo "session fail";
+    }
+}
+
+else {
+    ?>              <script>
+        alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+        history.back();
+    </script>
+    <?php
+}
+}
+/*else{
+    ?>              <script>
+        alert("아이디 혹은 비밀번호가 잘못되었습니다.");
+        history.back();
+    </script>
+    <?php
+}*/
+?>
